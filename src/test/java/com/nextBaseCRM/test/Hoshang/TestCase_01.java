f2fe0458731f4208b015510801c868022ca37b7a
hoshangwardak <hoshangwardak@gmail.com>
2021-01-20 01:10:32 -0500

    First Share

---
+++ src/test/java/com/nextBaseCRM/test/Hoshang/TestCase_01.java
@@ -0,0 +1,10 @@
+package com.nextBaseCRM.test.Hoshang;
+
+public class TestCase_01 {
+    public static void main(String[] args) {
+
+        // Hello World
+        System.out.println("Hello Universe");
+
+    }
+}
