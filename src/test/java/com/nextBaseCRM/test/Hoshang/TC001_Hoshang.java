ac427a04aefa7099560a14f939d17f49528d4810
hoshangwardak <hoshangwardak@gmail.com>
2021-01-28 11:25:45 -0500

    Added this test case in to the learningBranchWithAkbar

---
+++ src/test/java/com/nextBaseCRM/test/Hoshang/TC001_Hoshang.java
@@ -0,0 +1,8 @@
+package com.nextBaseCRM.test.Hoshang;
+
+public class TC001_Hoshang {
+    public static void main(String[] args) {
+
+        // I just created this class under LearningBranchWithAkbar
+    }
+}
