e70cc91b665917cdf1a91eb161da0799208336be
hoshangwardak <hoshangwardak@gmail.com>
2021-01-29 22:34:26 -0500

    Created SmartBear Test Cases

---
+++ src/test/java/com/nextBaseCRM/test/Hoshang/TC_9_10_11_SmartBearDeleteAndEditOrder.java
@@ -0,0 +1,16 @@
+package com.nextBaseCRM.test.Hoshang;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+
+public class TC_9_10_11_SmartBearDeleteAndEditOrder {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUpClass() {
+
+
+    }
+
+}
